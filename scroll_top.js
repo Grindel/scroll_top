d1f01781aee8123eb3146d6076c2063a8e307e5e
grindel <grindelman@gmail.com>
2013-10-08 01:33:23 +0400

    Add z-index

--- scroll_top.js
+++ scroll_top.js
@@ -1,6 +1,6 @@
 var scroller = document.createElement('div');
 scroller.id = "top_scroll_scroller"
 scroller.style.cssText = 'min-height: ' + document.documentElement.clientHeight + 'px;' +
-						 'width: 1px; position: fixed; top: 0; left: 0;'
+						 'width: 1px; position: fixed; top: 0; left: 0; z-index: 1000;'
 scroller.onclick = function() {scrollTo(0,0)}
 document.body.appendChild(scroller);
